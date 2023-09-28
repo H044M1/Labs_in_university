af63cd7e82c54dad8542abc0fec7a3861fc4e8f1
Anton <antondolganev54@gmail.com>
2023-09-28 19:23:35 +0700

    last version of practice2

---
+++ 3semester/ITIOP/practice2/struct.h
@@ -0,0 +1,12 @@
+#include <string>
+
+struct Number {
+	std::string num;
+	std::string exponent;
+};
+
+struct bytes_number {
+	std::string fourbyte;
+	std::string eightbyte;
+	std::string hex_num;
+};
